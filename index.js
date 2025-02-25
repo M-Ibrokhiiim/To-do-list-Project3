c34b8b9d8ff9cae1166288e96e0a1885ed918255
M_Ibrokhim <ibrokhimmuysinov@gmail.com>
2025-02-25 19:32:25 +0300

    50% finished project

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
- 
 // DOM
 
+const list=[]
+
 function valueTaking(){
+  
 
-    const list=[]
-    const inputValue=document.getElementById('add-section-inputId').value
-    const obj={toDo:inputValue}
+  let valueOfInput=document.getElementById('add-section-inputId').value.trim()
 
-    list.push(obj)
-    
-    let todos=''
-    list.forEach((listItem)=>{      
-    
-    todos+=`
-     <div class="list-of-items-main-container" id="list-of-items-main-containerId">
-            <div>
-             <input type="checkbox" name="radio-of-list" id="radio-of-list" class="list-of-items-input" id="list-of-items-inputId">
-             <label for="item" class="list-of-items-label" id="list-of-items-labelId">${listItem.toDo}</label>
+  if(valueOfInput===""){
+    alert('Please fill out!!!')
+    return
+  }
+  if(list.includes(valueOfInput)){
+    alert('Item exist!!!')
+    return
+  } 
+  
+  list.push(valueOfInput)
+  let UIitaration=''
+  list.forEach(item=>{
+      UIitaration +=`
+            <div class="list-of-items-main-container" id="list-of-items-main-containerId">
+                <div>
+                 <input type="checkbox" name="radio-of-list" id="radio-of-list" class="list-of-items-input" id="list-of-items-inputId">
+                 <label for="item" class="list-of-items-label" id="list-of-items-labelId">${item}</label>
+                </div>
+                <button class="list-of-items-btn" id="list-of-items-btnId">x</button>
             </div>
-            <button class="list-of-items-btn" id="list-of-items-btnId">x</button>
-          </div>
-    `
+      `})  
     
-    })
-    const listOfToDos=document.getElementById('list-of-items1')
-    listOfToDos.innerHTML=todos
+    const appealToUI=document.getElementById('list-of-items1')
+    appealToUI.innerHTML=UIitaration
+    valueOfInput=""
 }
+
